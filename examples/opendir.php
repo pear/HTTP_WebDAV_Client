124be5a426693d1eb26ed925bf12347dd8315515
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-04-18 13:01:20 +0930

    Clean up example code

--- examples/opendir.php
+++ examples/opendir.php
@@ -1,19 +1,18 @@
 <?php
-	// HTTP_WebDAV_Client registers the "webdav:" userland stream
-	require_once "HTTP/WebDAV/Client.php";
+// HTTP_WebDAV_Client registers the "webdav:" userland stream
+require_once "HTTP/WebDAV/Client.php";
 
-    // public webdav server as announced on 
-	// http://developer.novell.com/extend/director/1194.html
-	// WARNING: you should change this to your private server
-	//          as the test service seems to be broken right now
-	$name = "webdav://devcenter:rocks@webdav.silverstream.com/Director/WebDAVService/main/";
+// public webdav server as announced on 
+// http://developer.novell.com/extend/director/1194.html
+// WARNING: you should change this to your private server
+//          as the test service seems to be broken right now
+$name = "webdav://devcenter:rocks@webdav.silverstream.com/Director/WebDAVService/main/";
 
-    $dh = opendir($name);
-	if (!$dh) die("WebDAV connect failed");
+  $dh = opendir($name);
+if (!$dh) die("WebDAV connect failed");
 
-	while (false != ($file = readdir($dh))) {
-	  echo "$file\n";
-	}
+while (false != ($file = readdir($dh))) {
+  echo "$file\n";
+}
 
-	closedir($dh);
-?>
+closedir($dh);
