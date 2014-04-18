ae3e1da6803006ed04268bc700caf170334958b7
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-04-18 13:08:10 +0930

    Whitespace

--- HTTP/WebDAV/Client/Stream.php
+++ HTTP/WebDAV/Client/Stream.php
@@ -494,16 +494,16 @@ class HTTP_WebDAV_Client_Stream
 
             // for all returned resource entries
             foreach (explode("\n", $req->getResponseBody()) as $line) {
-            	// Preg_match_all if the whole response is one line!
+                // Preg_match_all if the whole response is one line!
                 if (preg_match_all("/href>([^<]*)/", $line, $matches)) {
                     // skip the directory itself                    
                     foreach ($matches[1] as $match){
-                    	// Compare to $this->url too
-	                    if ($match == "" || $match == $this->path || $match == $this->url) {
-	                    	continue;
-	                    }
-	                    // just remember the basenames to return them later with readdir()
-	                    $this->dirfiles[] = basename($match);
+                        // Compare to $this->url too
+                        if ($match == "" || $match == $this->path || $match == $this->url) {
+                            continue;
+                        }
+                        // just remember the basenames to return them later with readdir()
+                        $this->dirfiles[] = basename($match);
                     }
                 }
             }
